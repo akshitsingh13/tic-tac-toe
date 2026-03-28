d0647db312836b353292de7f2c8ad07660be0bd5
Akshit Singh <akshitsingh254@gmail.com>
2026-03-28 15:32:06 +0530

    Added win and draw conditions

--- script.js
+++ script.js
@@ -6,10 +6,21 @@ let player2 = "X";
 let currentPlayer = player1;
 let board = ["", "", "", "", "", "", "", "", ""];
 
+const winCondition = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+
+]
+
 function gameStart() {
     allBoxes.forEach(box => {
         box.addEventListener("click", function (e) {
-
             const index = e.target.dataset.index;
 
             if (board[index] !== "") return;
@@ -27,9 +38,27 @@ function gameStart() {
             e.target.style.backgroundSize = "cover";
             e.target.style.backgroundPosition = "center";
 
+            if (winCheck()) return;
+
+            if (board.every(cell => cell !== "")){
+                alert("Draw!");
+                return;
+            }
+
             currentPlayer = (currentPlayer === player1) ? player2 : player1;
         })
     })
+}
+
+function winCheck() {
+    for (let condition of winCondition) {
+        const [a, b, c] = condition;
+        if (board[a] !== "" && board[a] === board[b] && board[b] === board[c]) {
+            alert(board[a] + " wins!");
+            return true;
+        }
+    }
+    return false;
 }
 
 gameStart();
